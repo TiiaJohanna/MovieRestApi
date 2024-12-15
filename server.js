9a204e6ca972973d8021d6cd1b7c04f7f12c5304
TiiaJohanna <n3titi00@students.oamk.fi>
2024-12-15 18:23:09 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import pg from 'pg';
+import dontev from 'dotenv';
+dontev.config();
+
+
+const app = express();
+
+
+app.listen(3001, () => {
+    console.log('Server is running..');
+});
+
+app.get('/', (req,res) => {
+    res.send('You are at the frontpage of this server');
+});
